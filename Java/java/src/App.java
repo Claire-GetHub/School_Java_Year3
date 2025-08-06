905428d3b64c5d7a322ca1a6c35a1a6198121b22
Claire-GetHub <claire@execor.com>
2025-08-06 13:50:25 -0700

    Add files via upload

---
+++ Java/java/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(5 < 3);
+        System.out.println(9/5.0);
+    }
+}
