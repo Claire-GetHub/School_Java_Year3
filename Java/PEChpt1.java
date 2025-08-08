590d1589a71d82197c802657d38fb1ad1c1880b7
Claire-GetHub <claire@execor.com>
2025-08-08 14:06:45 -0700

    Add files via upload

---
+++ Java/PEChpt1.java
@@ -0,0 +1,22 @@
+public class PEChpt1 {
+    public static void main(String[] args) {
+        OnePointFive();
+    }
+
+    public static void OnePointOne() {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Compter Science");
+        System.out.println("Programming is fun");
+    }
+
+    public static void OnePointThree() {
+        System.out.println("    J     A   V     V     A");
+        System.out.println("    J    A A   V   V     A A");
+        System.out.println("J   J   AAAAA   V V     AAAAA");
+        System.out.println(" J J   A     A   V     A     A");
+    }
+
+    public static void OnePointFive() {
+        System.out.println((9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5));
+    }
+}
