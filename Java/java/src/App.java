c6a799a1104e1887efce6937fdc3a6f314e538df
Claire-GetHub <claire@execor.com>
2025-08-08 14:07:26 -0700

    Delete Java/java directory

--- Java/java/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println(5 < 3);
-        System.out.println(9/5.0);
-    }
-}
