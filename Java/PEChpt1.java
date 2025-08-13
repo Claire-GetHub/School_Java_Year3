6771147ae5ef74e704ac10db4cf7cc2623016f4e
Claire-GetHub <claire@execor.com>
2025-08-13 13:47:50 -0700

    Add files via upload

--- Java/PEChpt1.java
+++ Java/PEChpt1.java
@@ -1,6 +1,6 @@
 public class PEChpt1 {
     public static void main(String[] args) {
-        OnePointFive();
+        OnePointOne();
     }
 
     public static void OnePointOne() {
@@ -18,5 +18,28 @@ public class PEChpt1 {
 
     public static void OnePointFive() {
         System.out.println((9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5));
+    }
+
+    public static void OnePointSeven() {
+        System.out.println(4.0 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11));
+        System.out.println(4.0 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+    }
+
+    public static void OnePointNine() {
+        System.out.println(4.5 * 7.9);
+        System.out.println(4.5 * 2 + 7.9 * 2);
+    }
+
+    public static void OnePointEleven() {
+        System.out.println(312032486 + (1 * ((1.0/7 - 1.0/13 + 1.0/45) * 31536000)));
+        System.out.println(312032486 + (2 * ((1.0/7 - 1.0/13 + 1.0/45) * 31536000)));
+        System.out.println(312032486 + (3 * ((1.0/7 - 1.0/13 + 1.0/45) * 31536000)));
+        System.out.println(312032486 + (4 * ((1.0/7 - 1.0/13 + 1.0/45) * 31536000)));
+        System.out.println(312032486 + (5 * ((1.0/7 - 1.0/13 + 1.0/45) * 31536000)));
+    }
+
+    public static void OnePointThirteen() {
+        System.out.println("x: " + ((44.5 * .55 - 50.2 * 5.9)/(3.4 * .55 - 50.2 * 2.1)));
+        System.out.println("y: " + ((3.4 * 5.9 - 44.5 * 2.1)/(3.4 * .55 - 50.2 * 2.1)));
     }
 }
---
+++ Java/Variables.java
@@ -0,0 +1,46 @@
+
+
+public class Variables {
+    public static void main(String[] args) {
+        //USER INPUT
+        // import java.util.Scanner;
+        // Scanner input = new Scanner(System.in);
+
+        //primitive vars
+        // int
+        // double
+        // char
+
+        //numerical data types
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+
+        //Named Constants
+        //  final datatype CONSTANTNAME = VALUE;
+
+        //power
+        // Math.pow(2,3) is 2^3
+
+        //NUMERIC LITERALS
+        // 0B -> a number starting with 0B means the next number will be binary
+        // 0 or 0X -> the next numbers will be in hexadecimal
+        //
+        //1.0F -> turn 1.0, a double, into a float
+
+        ///////
+
+        // System.out.print("seconds: ");
+        // int seconds = input.nextInt();
+        // int minutes = seconds / 60;
+        // int secondsLeft = seconds % 60;
+
+
+        System.out.println(0XFFFF);
+        System.out.println(Math.pow(2.5, 2));
+        System.out.println(Math.pow(2.5, -2));
+    }
+}
