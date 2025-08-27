f6474d1a3c185be7660cd0d7ae4b89c1c0324941
Claire-GetHub <claire@execor.com>
2025-08-27 13:58:40 -0700

    Add files via upload

--- Java/Taxes.java
+++ Java/Taxes.java
@@ -5,48 +5,57 @@ public class Taxes {
         int status = 1;
         int income = 1000;
         
-        if (status == 1) {
-            if (income > 372951) {
-
-            } else if (income > 171551) {
-
-            } else if (income > 82251) {
-
-            } else if (income > 33951) {
-
-            } else if (income > 8351) {
-
-            } else if (income > 0) {
-                System.out.println("Your income is negative");
+        switch (status) {
+            case 0 -> {
             }
-        } else if (status == 2) {
-            if (income > 372951) {
+            case 1 -> {
+                if (income > 372951) {
 
-            } else if (income > 171551) {
-
-            } else if (income > 82251) {
-
-            } else if (income > 33951) {
-
-            } else if (income > 8351) {
-
-            } else if (income > 0) {
-                System.out.println("Your income is negative");
+                } if (income > 208851) {
+                    
+                } if (income > 137050) {
+                    
+                } if (income > 67900) {
+                    
+                } if (income > 16700) {
+                    
+                } if (income > 0) {
+                    
+                }
             }
-        } else if (status == 3) {
-            if (income > 372951) {
-
-            } else if (income > 171551) {
-
-            } else if (income > 82251) {
-
-            } else if (income > 33951) {
-
-            } else if (income > 8351) {
-
-            } else if (income > 0) {
-                System.out.println("Your income is negative");
+            case 2 -> {
+                if (income > 186476) {
+                    
+                } else if (income > 104426) {
+                    
+                } else if (income > 68526) {
+                    
+                } else if (income > 33951) {
+                    
+                } else if (income > 8351) {
+                    
+                } else if (income > 0) {
+                    
+                }
+            }
+            case 3 -> {
+                if (income > 372951) {
+                    
+                } else if (income > 190201) {
+                    
+                } else if (income > 117451) {
+                    
+                } else if (income > 45501) {
+                    
+                } else if (income > 11950) {
+                    
+                } else if (income > 0) {
+                    
+                }
+            }
+            default -> {
             }
         }
-    }
+
+            }
 }
