b7f503cebb86720875c01f1cdc2277455b31b67e
Claire-GetHub <claire@execor.com>
2025-09-17 14:36:04 -0700

    Add files via upload

--- Java/StringExercises.java
+++ Java/StringExercises.java
@@ -2,24 +2,90 @@
 import java.util.Scanner;
 
 public class StringExercises {
+    //Formula: F = m*g*sin(angle)
     public static void main(String[] args) {
-        fourPointOne();
+        // fourPointOne();
+        // fourPointTwo();
+        // fourPointThree();
+        // fourPointFour();
+        fourPointFive();
     }
 
     static void fourPointOne() {
         //Physics: ramp angle
-        //Formula: (F)
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            final double gravity = 9.8;
 
-        final double gravity = 9.8;
+            System.out.print("Enter the mass of the cart: ");
+            int mass = input.nextInt();
+            System.out.print("Enter the force to push the cart: ");
+            int force = input.nextInt();
 
-        System.out.print("Enter the mass of the cart: ");
-        int mass = input.nextInt();
-        System.out.print("Enter the force to push the cart: ");
-        int force = input.nextInt();
+            double angle = Math.toDegrees(Math.asin((force)/(mass*gravity)));
 
-        double angle;
+            System.out.printf("The angle of the ramp is %.1f degrees", angle);
+        }
+    }
 
+    static void fourPointTwo() {
+        //Physics: force to push a cart
+
+        try (Scanner input = new Scanner(System.in)) {
+            final double gravity = 9.8;
+
+            System.out.print("Enter the mass of the cart: ");
+            int mass = input.nextInt();
+            System.out.print("Enter the angle of the ramp: ");
+            int angle = input.nextInt();
+
+            double force = mass * gravity * Math.sin(Math.toRadians(angle));
+
+            System.out.printf("The force needed to push the cart is %.2f Newtons", force);
+        }
+    }
+
+    static void fourPointThree() {
+        //formula: if, sin > coefficient * cos, the brick will slide
+        // 
+        try (Scanner input = new Scanner(System.in)) {
+
+            System.out.print("Enter the coefficient of friction: ");
+            double coefficient = input.nextDouble();
+
+            double angle = Math.toDegrees(Math.atan(coefficient));
+
+            System.out.printf("The minimal angle for the brick to slide is %.3f degrees", angle);
+        }
+    }
+
+    static void fourPointFour() {
+        //acceleration = gravity * sin - coefficient* gravity * cos
+
+        try (Scanner input = new Scanner(System.in)) {
+            final double gravity = 9.8;
+
+            System.out.print("Enter the coefficient of friction: ");
+            double coefficient = input.nextDouble();
+            System.out.print("Enter the angle: ");
+            double rad = Math.toRadians(input.nextDouble());
+
+            double acceleration = (gravity * Math.sin(rad)) - (coefficient * gravity * Math.cos(rad));
+
+            if (acceleration > 0) {
+                System.out.printf("The brick accelerates at %.5f meters per square seconds", acceleration);
+            } else {
+                System.out.println("The brick does not move or move at a constant speed");
+            }
+        }
+    }
+
+    static void fourPointFive() {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a string: ");
+            String str = input.nextLine().trim();
+            Character lastChar = str.charAt(str.length() - 1);
+            System.out.print(lastChar);
+        }
     }
 }
